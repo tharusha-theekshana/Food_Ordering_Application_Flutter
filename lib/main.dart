e3158fdbbffba0e50cab7e6cf55fc5a1cecde691
Theekshana <theenutheekshana@gmail.com>
2025-02-21 20:20:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:food_order_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Order App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData.dark(
+        useMaterial3: true
+      ),
+      themeMode: ThemeMode.system,
+      home: HomeScreen()
+    );
+  }
+}
+
---
+++ lib/screens/spash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpashScreen extends StatefulWidget {
+  const SpashScreen({super.key});
+
+  @override
+  State<SpashScreen> createState() => _SpashScreenState();
+}
+
+class _SpashScreenState extends State<SpashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
